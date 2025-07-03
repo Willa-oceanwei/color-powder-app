632e1273243628f0b657aa8da771d0992181418b
Willa-oceanwei <ocean700120@gmail.com>
2025-07-03 14:44:24 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("色粉管理系統測試版")
+
+st.write("Hello, world!")
+
+# For testing reading secrets:
+# secret = st.secrets.get("my_test_secret", "未設定 secret")
+# st.write(f"Secret 值: {secret}")
