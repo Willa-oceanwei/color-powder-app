5796051d59180f91199248486655683f7fd0b5ff
Willa-oceanwei <ocean700120@gmail.com>
2025-07-21 09:39:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -334,7 +334,7 @@ elif menu == "配方管理":
     if pantone_kw:
        df_filtered = df_filtered[df_filtered["Pantone色號"].str.contains(pantone_kw, case=False, na=False)]
     
-        st.subheader("➕ 新增 / 修改配方")
+    st.subheader("➕ 新增 / 修改配方")
 
 # =================== 客戶名單選單與預設值 ===================
     try:
