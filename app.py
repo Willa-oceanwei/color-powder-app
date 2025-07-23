ef0372488a2240f43823c7fb4f86bf8cd0ef9c72
Willa-oceanwei <ocean700120@gmail.com>
2025-07-23 15:54:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -745,15 +745,17 @@ elif menu == "生產單管理":
     ws_order = spreadsheet.worksheet("生產單")
     df_recipe = pd.DataFrame(ws_recipe.get_all_records()).astype(str)
 
-    order_dir = Path("data")
-    os.makedirs(order_dir, exist_ok=True)  # 確保 data 資料夾存在
-    order_file = order_dir / "df_order.csv"
+    from pathlib import Path
 
+    order_dir = Path("data")
+    order_dir.mkdir(parents=True, exist_ok=True)  # 確保data資料夾存在
+
+    order_file = order_dir / "df_order.csv"
     if order_file.exists():
         df_order = pd.read_csv(order_file, dtype=str)
     else:
-        df_order = pd.DataFrame(columns=["生產單號", "生產日期", "配方編號", "顏色", "客戶名稱", "包裝重量", "包裝份數", "建立時間"])
-
+        df_order = pd.DataFrame(columns=["生產單號", "生產日期", "配方編號", "顏色",
+                                         "客戶名稱", "包裝重量", "包裝份數", "建立時間"])
     st.subheader("🧾 生產單管理")
 
     # 初始化 session_state 變數
