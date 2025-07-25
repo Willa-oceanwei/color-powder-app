8b15eae298f8864827af36ec7eb48886619a8691
Willa-oceanwei <ocean700120@gmail.com>
2025-07-26 02:20:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1165,7 +1165,7 @@ elif menu == "生產單管理":
             code_to_id[label] = row["生產單號"]
 
         selected_label = st.selectbox("選擇生產單號", options, key="selected_order_code_edit")
-        selected_code_edit = code_to_id.get(selected_label
+        selected_code_edit = code_to_id.get(selected_label)
                                             
 # 2. 在 selectbox 中顯示 options
 with cols_mod[0]:
