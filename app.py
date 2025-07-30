4db258cd5515c1575e35487e8e6f4992d54893f3
Willa-oceanwei <ocean700120@gmail.com>
2025-07-30 21:05:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -925,7 +925,7 @@ elif menu == "生產單管理":
         total_type = recipe_row.get("合計類別", "").strip() or "合計"
     
         powder_label_width = 8    # 色粉代號欄位寬度
-        col_width = 18            # 每一個重量欄位寬度
+        col_width = 14            # 每一個重量欄位寬度
         indent = " " * 15         # 包裝列縮排
     
         colorant_ids = [recipe_row.get(f"色粉編號{i+1}", "") for i in range(8)]
