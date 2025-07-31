2260e9f1d876cc8ce5a2c9c8569bc86c05883e68
Willa-oceanwei <ocean700120@gmail.com>
2025-07-31 10:35:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -927,7 +927,7 @@ elif menu == "生產單管理":
         pack_col_width = 14       # 包裝列每個欄位寬度（固定不動）
         number_col_width = 6      # 每個數字欄位寬度（數字本身寬度）
         base_offset = 0           # 微調整體左右位置
-        column_offsets = [1, 8.5, 7, 8]
+        column_offsets = [1, 8.5, 8.5, 9.5]
     
         packing_indent = " " * 14  # 包裝列縮排
         lines = []
