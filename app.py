8e70eea7f457144c20dec60b205b535456de871f
Willa-oceanwei <ocean700120@gmail.com>
2025-08-07 17:10:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1352,7 +1352,7 @@ elif menu == "生產單管理":
             lines.append("＿" * 30)
                     
         # 合計列
-        if total_type == "無":
+        if total_type == "" or total_type == "無":
             total_type_display = f"<b>{'共'.ljust(powder_label_width)}</b>"
         elif category == "色母":
             total_type_display = f"<b>{'料'.ljust(powder_label_width)}</b>"
