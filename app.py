67817d697d207459c58a38c644f7e530e725d572
Willa-oceanwei <ocean700120@gmail.com>
2025-08-08 09:51:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -708,7 +708,7 @@ elif menu == "配方管理":
                 if key.startswith("form_recipe_") or key.startswith("ratio"):
                     st.session_state.pop(key, None)
         
-            st.experimental_rerun()
+            st.rerun()
     
     # === 表單提交後的處理邏輯（要在 form 區塊外） ===
     if submitted:
