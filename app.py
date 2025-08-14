6cae00ddf27f32d0869e137a22d99e5cc08a5062
Willa-oceanwei <ocean700120@gmail.com>
2025-08-14 12:34:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1096,7 +1096,8 @@ elif menu == "生產單管理":
         if "原始配方" in df_recipe.columns:
             df_recipe["原始配方"] = df_recipe["原始配方"].map(clean_powder_id)
     
-        # ✅ 清理完後再比對附加配方
+        main_recipe_code_raw = order.get("配方編號", "")
+        main_recipe_code = fix_leading_zero(clean_powder_id(main_recipe_code_raw))
         matched_additional = df_recipe[df_recipe["原始配方_標準"] == main_recipe_code]
     
         st.session_state.df_recipe = df_recipe
