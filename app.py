528965aaf9bc430dba648921e9d664b70c8d8f55
Willa-oceanwei <ocean700120@gmail.com>
2025-09-04 16:41:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2530,6 +2530,8 @@ if menu == "交叉查詢區":
                 })
 
             df_result = pd.DataFrame(results)
+            if "index" in df_result.columns:
+                df_result = df_result.drop(columns=["index"])
             st.dataframe(df_result.reset_index(drop=True), use_container_width=True)
 
 # ===== 匯入配方備份檔案 =====
