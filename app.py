ce79dbdd9fec913e4f7d37674bab5be30d638679
Willa-oceanwei <ocean700120@gmail.com>
2025-09-10 14:10:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -781,8 +781,8 @@ elif menu == "配方管理":
     <div id="recipe-create" style="display: flex; align-items: center; gap: 10px;">
         <div class="big-title">🎯配方建立</div>
         <a href="#recipe-table" style="
-            background-color:#f0f2f6;   /* 灰底 */
-            color:#31333F;             /* 深灰字 */
+            background-color: var(--background-color);
+            color: var(--text-color);
             padding:4px 10px;
             border-radius:6px;
             text-decoration:none;
