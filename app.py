8afada87162f496ad1f16a8fa2c15cb3ab29c038
Willa-oceanwei <ocean700120@gmail.com>
2025-09-10 11:52:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -958,8 +958,8 @@ elif menu == "配方管理":
 
         /* 調整 text_input 外層 margin */
         div.stTextInput {
-            margin-top: -6px !important;
-            margin-bottom: -6px !important;
+            margin-top: -8px !important;
+            margin-bottom: -8px !important;
         }
 
         /* 調整 columns row 的 gap */
