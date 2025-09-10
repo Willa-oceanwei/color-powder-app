79ddd766f3e77da430629feae20f5ffba870de28
Willa-oceanwei <ocean700120@gmail.com>
2025-09-10 09:57:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1231,7 +1231,7 @@ elif menu == "配方管理":
     st.markdown("---")  # 分隔線
 
     st.markdown(
-        '<h2 style="font-size:26px; font-family:Arial; color:#dbd818;">🔧 配方修改/刪除</h2>',
+        '<h2 style="font-size:26px; font-family:Arial; color:#dbd818;">🔩 配方修改/刪除</h2>',
         unsafe_allow_html=True
     )
     
