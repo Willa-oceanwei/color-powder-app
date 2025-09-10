9db55f4b1c8d5cf32b32d42b50f3e91b312fd7cc
Willa-oceanwei <ocean700120@gmail.com>
2025-09-10 16:21:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2036,7 +2036,7 @@ elif menu == "生產單管理":
     # ---------- 生產單清單 + 修改 / 刪除 ----------
     st.markdown("---")
     st.markdown(
-        '<h2 style="font-size:26px; font-family:Arial; color:#dbd818;">📑 生產單記錄表</h2>',
+        '<h2 style="font-size:26px; font-family:Arial; color:#F9DC5;">📑 生產單記錄表</h2>',
         unsafe_allow_html=True
     )
     
