c44c487529c6e11e1514d0ff089986f76af49c23
Willa-oceanwei <ocean700120@gmail.com>
2025-09-25 09:42:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1668,12 +1668,7 @@ elif menu == "配方管理":
         st.session_state.df_recipe = load_recipe_data()
         st.success("配方資料已重新載入！")
         st.experimental_rerun()
-        # 頁面最下方手動載入按鈕
-        st.markdown("---")
-        if st.button("📥 重新載入配方資料"):
-            st.session_state.df_recipe = load_recipe_data()
-            st.success("配方資料已重新載入！")
-            st.experimental_rerun()  # 重新載入頁面，更新資料
+        
             
     # --- 生產單分頁 ----------------------------------------------------
 elif menu == "生產單管理":
