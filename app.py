a0e01378fe09bacccdf452eeb15c126a10293ce6
Willa-oceanwei <ocean700120@gmail.com>
2025-10-07 15:01:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1278,7 +1278,7 @@ elif menu == "配方管理":
 
     st.markdown("""
     <div id="recipe-table" style="display: flex; align-items: center; gap: 10px;">
-        <h2 style="font-size:24px; font-family:Arial; color:#dbd818;">📑配方記錄表</h2>
+        <h2 style="font-size:24px; font-family:Arial; color:#F9DC5C;">📑配方記錄表</h2>
         <a href="#recipe-create" style="
             background-color: var(--background-color);  /* 跟隨亮/暗模式 */
             color: var(--text-color);                  /* 跟隨亮/暗模式 */
