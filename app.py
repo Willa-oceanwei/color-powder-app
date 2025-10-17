b4e522e456b091e48d9f95b5c7c1160052e0e9d6
Willa-oceanwei <ocean700120@gmail.com>
2025-10-17 11:26:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3746,7 +3746,7 @@ if menu == "庫存區":
     # 確保查詢日期 key 不同，避免衝突
     query_start = col1.date_input("查詢起日", key="stock_start_query") 
     query_end = col2.date_input("查詢迄日", key="stock_end_query")
-    stock_powder = st.text_input("色粉編號 (01以下請選擇日期，或至用量區查詢)", key="stock_powder")
+    stock_powder = st.text_input("色粉編號 (01以下請選擇日期，或至「交叉查詢區」➔色粉用量查詢)", key="stock_powder")
 
     # 初始化 session_state
     if "ini_dict" not in st.session_state:
