2b0b7a5ee5b353e6742eecfcb991f836e18ea671
Willa-oceanwei <ocean700120@gmail.com>
2025-10-20 14:27:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3753,7 +3753,7 @@ if menu == "庫存區":
     st.markdown(f"""
         <style>
         div[data-testid="stTextInput"][data-baseweb="input"] > div:has(input#st-{input_key}) {{
-            margin-top: -28px !important;
+            margin-top: -32px !important;
         }}
         </style>
 
