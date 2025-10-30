776df913efd4fc12ee682b524653c407c36c7cc1
Willa-oceanwei <ocean700120@gmail.com>
2025-10-30 14:14:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -786,7 +786,7 @@ elif menu == "客戶名單":
                 c1, c2, c3 = st.columns([3,1,1])
                 with c1:
                     st.markdown(
-                        f"<div style='font-family:Arial;color:#FFFFFF;'>🤖 {row['客戶編號']}　{row['客戶簡稱']}</div>",
+                        f"<div style='font-family:Arial;color:#FFFFFF;'>🔹 {row['客戶編號']}　{row['客戶簡稱']}</div>",
                         unsafe_allow_html=True
                     )
                 with c2:
