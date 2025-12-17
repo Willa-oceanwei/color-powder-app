86442c5e2d48f0c81fbef32b640b639c8c0255a1
Willa-oceanwei <ocean700120@gmail.com>
2025-12-17 15:21:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ if "authenticated" not in st.session_state:
 # 尚未登入時，顯示登入介面
 if not st.session_state.authenticated:
 	st.markdown(
-		"<h3 style='text-align:center; color:#f0efa2;'>🔐 請輸入密碼</h3>",
+		"<h3 style='text-align:center; color:#f0efa2;'>👻 請輸入密碼 👻</h3>",
 		unsafe_allow_html=True,
 	)
 
