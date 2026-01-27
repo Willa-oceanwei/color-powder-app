09e27671ce5d2dde6716f98083d7cc35e4cd9b1b
Willa-oceanwei <ocean700120@gmail.com>
2026-01-27 10:32:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3663,11 +3663,11 @@ elif menu == "生產單管理":
                 /* 調整 selectbox 與 number_input 字體和高度 */
                 div[data-baseweb="select"] > div {
                     font-size: 12px !important;
-                    min-height: 20px !important;
+                    min-height: 15px !important;
                 }
                 div.stNumberInput > label > div > input {
                     font-size: 12px !important;
-                    height: 20px !important;
+                    height: 15px !important;
                     padding: 2px 4px !important;
                 }
                 </style>
